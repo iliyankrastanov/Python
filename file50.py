e725b0fafc525aa56b6ba8ed1476b56d4bfd7ad3
Iliyan <iliyan97@abv.bg>
2021-04-20 16:58:48 +0300

    Python Exercises

---
+++ file50.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+    pow2 = lambda a: a ** 2
+    pown = lambda a,n: a ** n
+
+    print(f" 3 ** 2 = {pow2(3)}")
+    print(f" 3 ** 3 = {pown(3,3)}")
+    print("--- --- --- ---")
+
+#lambda izraza trqbva da e na 1 red
+#izraz koito vrushta stoinost bez "return"
+#moje da ima cikli no trqbva da sa s ciklichen izraz(na 1 red)
