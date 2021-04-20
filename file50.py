ffaf9a515255f18d391b9df2b2775368c05f4a7a
Iliyan <iliyan97@abv.bg>
2021-04-20 17:06:16 +0300

    Python Exercises

--- file50.py
+++ file50.py
@@ -6,11 +6,14 @@ if __name__ == "__main__":
     print(f" 3 ** 2 = {pow2(3)}")
     print(f" 3 ** 3 = {pown(3,3)}")
 
-    items = [('A', 5, 7),('D', 2, 6), ('B', 4, 5), ('D', 2, 5)]
-    items.sort()
-    print(f"items: {items}")
-    print("--- --- --- ---")
-
 #lambda izraza trqbva da e na 1 red
 #izraz koito vrushta stoinost bez "return"
 #moje da ima cikli no trqbva da sa s ciklichen izraz(na 1 red)
+
+    items = [('A', 5, 7),('D', 2, 6), ('B', 4, 5), ('D', 2, 5)]
+
+    items.sort(key = lambda el: (el[1],el[0],el[2]))
+    print(f"items: {items}")
+    print("--- --- --- ---")
+
+
