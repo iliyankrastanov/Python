104521f23dab052487fa1d1288e7fa88b8bf7a63
Iliyan <iliyan97@abv.bg>
2021-04-22 13:23:33 +0300

    Homework 2

---
+++ sumarr.py
@@ -0,0 +1,14 @@
+
+def arr_sum(arr):
+     if len(arr) == 1:
+        return arr[0]
+     else:
+        return arr[0]+ arr_sum(arr[1:])
+
+
+if __name__ == "__main__":
+
+    arr = [1, 2, 3, [5, 6, [7]], 4]
+res = arr_sum([[1,2,3,4], [5,6], [7]]) #Не мога да се сетя за друг вариант
+
+print (arr_sum(res))
